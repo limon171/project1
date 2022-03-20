a7a073bd3a465c68cf0916ed9c45e5d5f941cfac
Kirill <limon17141@gmail.com>
2022-03-20 15:56:09 +0200

    script.js mod

--- js/script.js
+++ js/script.js
@@ -1,25 +1,19 @@
-"use strict";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-console.log('arr' + " - object");
-console.log(4 + "5");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-let incr = 10,
-    decr = 10;
+const a = prompt ('Один из последних просмотренных фильмов?'),
+      b = prompt ('На сколько оцените его?', ''),
+      c = prompt ('Один из последних просмотренных фильмов?',''),
+      d = prompt ('На сколько оцените его?','');
 
-// ++incr;
-// --decr;
+      personalMovieDB.movies[a] = b;
+personalMovieDB.movies[a] = d;
 
-console.log(++incr);
-console.log(--decr);
-
-console.log(5%2);
-
-console.log(2 + 2 * 2 !== '6');
-
-&&
-||
-
-const isChecked = false,
-      isClose = false;
-
-console.log(isChecked || !isClose);
+console.log(personalMovieDB);
